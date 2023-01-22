15693ca8c4dac705c1ba24be9870be4a4c4d0510
Sébastien MB <seb@essembeh.org>
2023-01-22 19:31:46 +0100

    Better install command
    - upgrade outdated extensions
    - enable extensions which are disabled

--- gnome_extensions_cli/commands/install.py
+++ gnome_extensions_cli/commands/install.py
@@ -7,6 +7,7 @@ from argparse import ONE_OR_MORE, ArgumentParser, Namespace
 from ..icons import Color, Icons, Label
 from ..manager import ExtensionManager
 from ..store import GnomeExtensionStore
+from ..utils import version_comparator
 
 
 def configure(parser: ArgumentParser):
@@ -28,6 +29,7 @@ def run(args: Namespace, manager: ExtensionManager, store: GnomeExtensionStore):
     Handler for subcommand
     """
     installed_extensions = {e.uuid: e for e in manager.list_installed_extensions()}
+    enabled_uuids = manager.list_enabled_uuids()
     shell_version = manager.get_current_shell_version()
 
     for motif, available_ext in store.iter_fetch(
@@ -35,7 +37,22 @@ def run(args: Namespace, manager: ExtensionManager, store: GnomeExtensionStore):
     ):
         if available_ext is None:
             print(Icons.ERROR, "Cannot find extension", Color.RED(motif))
-        elif available_ext.uuid in installed_extensions:
+        elif available_ext.uuid not in installed_extensions:
+            print(Icons.PACKAGE, "Install", Label.available(available_ext))
+            manager.install_extension(available_ext)
+        elif (
+            version_comparator(
+                installed_extensions[available_ext.uuid].metadata.version,
+                available_ext.version,
+            )
+            > 0
+        ):
+            print(Icons.PACKAGE, "Upgrade", Label.available(available_ext))
+            manager.install_extension(available_ext)
+        elif available_ext.uuid not in enabled_uuids:
+            print(Icons.HINT, "Enable", Label.available(available_ext))
+            manager.enable_uuids(available_ext.uuid)
+        else:
             print(
                 Icons.DRYRUN,
                 "Extension",
@@ -43,6 +60,3 @@ def run(args: Namespace, manager: ExtensionManager, store: GnomeExtensionStore):
                 "is already installed",
             )
             manager.enable_uuids(available_ext.uuid)
-        else:
-            print(Icons.PACKAGE, "Install", Label.available(available_ext))
-            manager.install_extension(available_ext)
